c564c55b6a996f506626cd4b0b80d2dad7c0fc24
ichi <realkouu@gmail.com>
2026-02-06 09:19:46 +0900

    Initial commit

---
+++ PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by 福田光一郎 on 2026/02/05.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
